fe6a6a716a0a00b5037bc86e29dc5a3ef5d32c4f
Muhammad Zain <rubencallen689@gmail.com>
2024-05-02 18:39:56 +0500

    Add currency conversion functionality

---
+++ coverter.js
@@ -0,0 +1,35 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: 'from',
+        message: 'Enter from currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'to',
+        message: 'Enter to currency',
+        type: 'list',
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: 'amount',
+        message: 'Enter your amount',
+        type: 'number'
+    },
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount;
+//                5000    usd          
+let convertedAmount = baseAmount * toAmount;
+//                                   pkr
+console.log(convertedAmount);
